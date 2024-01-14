3017da71c2c53d4d0660ff3d2a3692d854039230
aidaBC <aidabarros12024@gmail.com>
2024-01-14 21:03:57 +0100

    Project Creation

---
+++ global_2024/Source/global_2024/global_2024GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "global_2024GameModeBase.h"
+
---
+++ global_2024/Source/global_2024/global_2024GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "global_2024GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBAL_2024_API Aglobal_2024GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
