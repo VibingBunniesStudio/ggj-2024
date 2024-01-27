2079efb7a140e2795a541616af1b582be95e7855
Óscar Carmona del Barrio <o.carmonadb@gmail.com>
2024-01-27 13:23:43 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/VibingBunniesStudio/ggj-2024""
    This reverts commit 17e8e5f4c368677fa12ff2380d340d8733f09e55.

--- global_2024/Source/global_2024/Managers/RoundManager.cpp
+++ global_2024/Source/global_2024/Managers/RoundManager.cpp
@@ -1,27 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #include "RoundManager.h"
-
 #include "Kismet/GameplayStatics.h"
+#include "InputCombo/InputComboManager.h"
 
-#pragma region ROUND_MANAGING
-void RoundManager::StartNewRound()
+// Sets default values
+ARoundManager::ARoundManager() : m_currentRound(-1)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ARoundManager::StartNewRound()
+{
+	m_currentRound++;
+	if (m_comboManager)
+	{
+		m_comboManager->GenerateComboSecuence(5, 4);
+	}
+}
+
+void ARoundManager::ApplyRoundResult(int winnerPlayerIndex)
 {
 }
 
-void RoundManager::ApplyRoundResult(int winnerPlayerIndex)
+void ARoundManager::FinishCombat()
 {
+}
+
+// Called when the game starts or when spawned
+void ARoundManager::BeginPlay()
+{
+	Super::BeginPlay();
+	m_comboManager = Cast<AInputComboManager>(UGameplayStatics::GetActorOfClass(GetWorld(), AInputComboManager::StaticClass()));
 	
 }
 
-void RoundManager::FinishCombat()
+// Called every frame
+void ARoundManager::Tick(float DeltaTime)
 {
-}
-#pragma endregion
+	Super::Tick(DeltaTime);
 
-RoundManager::RoundManager()
-{
 }
 
-RoundManager::~RoundManager()
-{
-}
