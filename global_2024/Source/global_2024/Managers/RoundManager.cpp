fabd565346a8db9d60b9ff9bbd1549e6907d7507
aidaBC <aidabarros12024@gmail.com>
2024-01-27 11:31:04 +0100

    Round manager

---
+++ global_2024/Source/global_2024/Managers/RoundManager.cpp
@@ -0,0 +1,27 @@
+
+#include "RoundManager.h"
+
+#include "Kismet/GameplayStatics.h"
+
+#pragma region ROUND_MANAGING
+void RoundManager::StartNewRound()
+{
+}
+
+void RoundManager::ApplyRoundResult(int winnerPlayerIndex)
+{
+	
+}
+
+void RoundManager::FinishCombat()
+{
+}
+#pragma endregion
+
+RoundManager::RoundManager()
+{
+}
+
+RoundManager::~RoundManager()
+{
+}
